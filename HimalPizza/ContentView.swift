fc4ba95b8b72d16581ed1c96ba49becc938a9c83
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-02-02 17:40:06 +0545

    inital commit

--- HimalPizza/ContentView.swift
+++ HimalPizza/ContentView.swift
@@ -8,12 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    var orders:[Int] = []
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            ZStack {
+                
+                Image("banner")
+                    .resizable()
+                .scaledToFit()
+               
+                ZStack{
+                    Text("Himal's Pizza")
+                        .background()
+                }
+            }
+            Image(systemName: orders.isEmpty ? "cart":
+            "cart.circle.fill")
+            HStack {
+              
+                Text("Order Pizza")
+                    .font(.title)
+                Spacer()
+            }
+               
+            HStack(alignment:.firstTextBaseline){
+                Text("Your Order Item")
+                Spacer()
+                Text(19.90, format: .currency(code: "NPR"))
+            }
+            VStack {
+                Image(systemName: "rectangle.fill")
+                    .resizable()
+                .frame(width: 50, height: 50)
+                
+                Text("Margherita")
+                Text("Discription")
+            }
+            HStack(alignment:.top){
+                Image(systemName: "1.circle.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack(alignment:.leading){
+                    Text("Margherita")
+                    Text("Discription")
+                }
+                
+            }
+            
+            Spacer()
         }
         .padding()
     }
