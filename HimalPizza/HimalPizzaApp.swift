291ea09d63796ec93ce715934a9c5598267bc188
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-02-02 16:41:10 +0545

    Initial Commit

---
+++ HimalPizza/HimalPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HimalPizzaApp.swift
+//  HimalPizza
+//
+//  Created by Paurakh Vikram Saud on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HimalPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
